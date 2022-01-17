4f2ac1e59f0d69262f07581383b3013ac899725f
ganqy <735990477@qq.com>
2022-01-17 19:23:43 +0800

    -

--- Solution.py
+++ Solution.py
@@ -9,8 +9,24 @@ class Solution:
                 a[num] = i
             else:
                 return [i, a[target - num]]
-    #5
-    
+    #9
+    def isPalindromeI(self, x: int) -> bool:
+        num = str(x)
+        for i in range((len(num) + 1) // 2):
+            if num[i] != num[len(num) - i - 1]:
+                return False
+        return True
+    def isPalindromeII(self, x: int) -> bool:
+        if x == 0:
+            return True
+        if x < 0 or x % 10 == 0:
+            return False
+        left = x
+        born = 0
+        
+        
+
+
 
              
 if __name__ == '__main__':
